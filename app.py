5bc17e1318558815fa934f1b047d04d2184cd935
cheshire <siamskymug@yandex.kz>
2026-09-21 22:19:59 +0300

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,34 @@ from fastapi import FastAPI
 
 app = FastAPI(title="My First Project")
 
+messages_db = {0: "First post in FastAPI"}
 
-@app.get("/example")
-async def example():
-    return {"message": "Hello"}
+
+@app.get("/messages")
+async def read_messages() -> dict:
+    pass
+
+
+@app.get("/messages/{message_id}")
+async def read_message(message_id: int) -> str:
+    pass
+
+
+@app.post("/messages")
+async def create_message(message: str) -> str:
+    pass
+
+
+@app.put("/messages/{message_id}")
+async def update_message(message_id: int, message: str) -> str:
+    pass
+
+
+@app.delete("/messages/{message_id}")
+async def delete_message(message_id: int) -> str:
+    pass
+
+
+@app.delete("/messages")
+async def delete_messages() -> str:
+    pass
