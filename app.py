09690933144dfab757c93b404841edbd28a5d809
cheshire <siamskymug@yandex.kz>
2026-09-22 23:59:40 +0300

    added get requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(title="My First Project")
 
@@ -7,12 +7,17 @@ messages_db = {0: "First post in FastAPI"}
 
 @app.get("/messages")
 async def read_messages() -> dict:
-    pass
+    return messages_db
 
 
 @app.get("/messages/{message_id}")
 async def read_message(message_id: int) -> str:
-    pass
+    try:
+        return messages_db[message_id]
+    except:
+        raise HTTPException(
+            status_code=404, detail="Message not found"
+        )
 
 
 @app.post("/messages")
